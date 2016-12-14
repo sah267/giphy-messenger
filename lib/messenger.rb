32c3071a91250505fa5d368c6915e2f298649c04
Habib, Stephanie <Stephanie.Habib@hasbro.com>
2016-12-14 18:53:28 -0500

    clean up and add feedback messages

--- lib/messenger.rb
+++ lib/messenger.rb
@@ -1,4 +1,3 @@
-require 'yaml'
 require 'twilio-ruby'
 
 class Messenger
@@ -10,36 +9,27 @@ class Messenger
     account_sid = 'ACa96662fb03827c636c41623c32a21e91'
     auth_token  = 'd7460700606e9a04bd7e4dff4e3f3c9c'
     @client = Twilio::REST::Client.new(account_sid, auth_token)
-
-    puts @client
   end
 
   def send_sms_notifications(phone_number, message, image_url)
-    puts 'send message'
-
-    alert_message = 'Hello'
-    # image_url = 'http://howtodocs.s3.amazonaws.com/new-relic-monitor.png'
-
     send_sms(phone_number, message, image_url)
-
-      # admins = YAML.load_file('config/administrators.yml')
-    # admins.each do |admin|
-    #   phone_number = admin['phone_number']
-    #   send_sms(phone_number, alert_message, image_url)
-    # end
   end
 
   private
 
   def send_sms(phone_number, message, image_url)
-    puts @client
 
-    @client.account.messages.create(
-        from:      '+17745411130',
-        to:        phone_number,
-        body:      message,
-        media_url: image_url
-        # media_url: image_url
-    )
+    begin
+      @client.account.messages.create(
+          from:      '+17745411130',
+          to:        phone_number,
+          body:      '/giphy' + message,
+          media_url: image_url
+      )
+    rescue
+      return 'error'
+    else
+      return 'success'
+    end
   end
 end
--- myapp.rb
+++ myapp.rb
@@ -7,41 +7,47 @@ require_relative './lib/messenger'
 
 
 get '/' do
-  erb :index, locals: {title: 'My Twilio App'}
-end
-
-get '/giphy' do
-  uri = URI('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC')
-
-
-  giphy = Net::HTTP.get(uri)
-  parsed = JSON.parse(giphy)
-
-  image = parsed['data']['image_original_url']
-
-  erb :giphy, locals: {image: image}
+  erb :index, locals: {title: 'My Twilio App', message: '', result: 'success'}
 end
 
 get '/send' do
 
   phone_number = params[:phone]
-  message = params[:message]
+  giphy_tag = params[:tag]
 
   uri = 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC'
 
-  if message
-    uri += '&tag=' + message
+  if giphy_tag
+    uri += '&tag=' + giphy_tag
   end
 
   uri = URI(uri)
 
   giphy = Net::HTTP.get(uri)
+
   parsed = JSON.parse(giphy)
 
-  image = parsed['data']['image_original_url']
+  if parsed['data'].kind_of?(Array)
+    puts 'does not exist'
 
-  Messenger.send_sms_notifications(phone_number, message, image)
+    test = 'No Giphs Found!'
+    result = 'error'
+  else
+    puts 'it exists!'
 
-  'Sent'
+    image = parsed['data']['image_original_url']
+
+    test = Messenger.send_sms_notifications(phone_number, giphy_tag, image)
+
+    if test == 'success'
+      message = 'Success! Giphy sent successfully!'
+      result = 'success'
+    else
+      message = 'Error: Sorry Something went wrong'
+      result = 'error'
+    end
+  end
+
+  erb :index, locals: {title: 'My Twilio App', message: message, result: result}
 
 end
